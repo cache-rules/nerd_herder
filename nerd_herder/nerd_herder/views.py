e93aabe29b863683ab1eb5ddb0f0ba293923523a
Alan Vezina <alan@fancysandwiches.com>
2018-01-27 14:39:09 -0800

    Remove old django app upgrading to Django 2

--- nerd_herder/nerd_herder/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    # TODO: determine what should go on index page.
-    #   - Maybe just redirect to talk submit page?
-    #   - Maybe guide organizers to login and potential speakers to submit talk page?
-    #   - Maybe have index page be information about the meetup?
-    return HttpResponse('Welcome to Nerd Herder')
