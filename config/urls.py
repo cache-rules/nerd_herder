edf7db9b3f80e341bc93d1d829adb68fb580b796
Alan Vezina <alan@fancysandwiches.com>
2018-05-14 22:36:06 -0700

    Remove index view.
    It didn't actually provide anything useful. In the future we'll set up
    nginx to load our React frontend at the index page and it will just
    proxy to the Django backend.

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from nerd_herder.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
 ]
 
 if settings.DEBUG:
--- nerd_herder/views.py
+++ nerd_herder/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render
 
-
-def index(request):
-    if request.user.is_authenticated:
-        return redirect(reverse('dashboard'), permanent=False)
-
-    return render(request, 'index.html')
+# Create your views here.
