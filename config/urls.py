0a16f6c367dd078901032ecaee620ea55ef168e8
Alan Vezina <alan@fancysandwiches.com>
2018-05-28 15:22:41 -0700

    Add code of conduct routes & views

--- config/urls.py
+++ config/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from nerd_herder.talks import urls as talks_urls
+from nerd_herder.views import index, code_of_conduct, code_of_conduct_reporting_guide,\
+    code_of_conduct_response_playbook
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('code-of-conduct', code_of_conduct),
+    path('code-of-conduct-reporting-guide', code_of_conduct_reporting_guide),
+    path('code-of-conduct-response-playbook', code_of_conduct_response_playbook),
+    path('api/talks', include(talks_urls, namespace='talks')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls, namespace='debug_toolbar')),
-    ] + urlpatterns
--- nerd_herder/views.py
+++ nerd_herder/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def code_of_conduct(request):
+    return render(request, 'code_of_conduct.html', {'title': 'Code of Conduct'})
+
+
+def code_of_conduct_reporting_guide(request):
+    context = {'title': 'Code of Conduct - Reporting Guide'}
+    return render(request, 'code-of-conduct-reporting-guide.html', context)
+
+
+def code_of_conduct_response_playbook(request):
+    context = {'title': 'Code of Conduct - Response Playbook'}
+    return render(request, 'code-of-conduct-response-playbook.html', context)
+
+
+def index(request):
+    return render(request, 'index.html')
