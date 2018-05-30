e2ac5c267b868d18b28839734a012237a9d9730f
Alan Vezina <alan@fancysandwiches.com>
2018-05-30 00:36:42 -0700

    Remove /app redirect from index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,4 @@ import './index.css';
 import './forms.css';
 import App from './App';
 
-if (window.location.pathname === '/') {
-  window.location = '/app';
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
