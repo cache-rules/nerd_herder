4512eece17ae7ad0d9f0604b15c9f361734003de
Alan Vezina <alan@fancysandwiches.com>
2018-06-03 20:26:35 -0700

    Add app view, load CSS file on code of conduct pages

--- nerd_herder/code_of_conduct/views.py
+++ nerd_herder/code_of_conduct/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
 
+from nerd_herder.utils import get_asset_urls
 from .models import CommitteeMember
 
 
 def code_of_conduct(request):
-    return render(request, 'code_of_conduct.html', {'title': 'Code of Conduct'})
+    _, css_url = get_asset_urls(request)
+    context = {'title': 'Code of Conduct', 'css_url': css_url}
+    return render(request, 'code_of_conduct.html', context)
 
 
 def code_of_conduct_reporting_guide(request):
+    _, css_url = get_asset_urls(request)
     context = {
-        'title': 'Code of Conduct - Reporting Guide',
+        'title': 'Code of Conduct',
+        'css_url': css_url,
         'committee_members': CommitteeMember.objects.all(),
     }
-
-    print(CommitteeMember.objects.all())
 
     return render(request, 'code-of-conduct-reporting-guide.html', context)
 
 
 def code_of_conduct_response_playbook(request):
-    context = {'title': 'Code of Conduct - Response Playbook'}
+    _, css_url = get_asset_urls(request)
+    context = {'title': 'Code of Conduct - Response Playbook', 'css_url': css_url}
     return render(request, 'code-of-conduct-response-playbook.html', context)
