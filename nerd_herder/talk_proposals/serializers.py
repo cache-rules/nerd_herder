d1d5ed9653e7634ec3c821f8b605b84dc893a576
Alan Vezina <alan@fancysandwiches.com>
2018-06-12 23:01:01 -0700

    Remove useless override of create method

--- nerd_herder/talk_proposals/serializers.py
+++ nerd_herder/talk_proposals/serializers.py
@@ -4,12 +4,6 @@ from .models import TalkProposal
 
 
 class TalkProposalSerializer(ModelSerializer):
-    def create(self, validated_data):
-        instance = super().create(validated_data)
-        # TODO: call slack webhook
-        # TODO: send email to organizers
-        return instance
-
     class Meta:
         model = TalkProposal
         fields = ('name', 'email', 'title', 'description', 'talk_type')
