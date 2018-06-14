28dcc4e8e3c775415519da6ef5a76c947ce7713a
Alan Vezina <alan@fancysandwiches.com>
2018-06-14 00:53:18 -0700

    Add utils method to send new talk proposal notifications

---
+++ nerd_herder/talk_proposals/utils.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+
+from nerd_herder.slack.messages import send_message
+from nerd_herder.talk_proposals.models import TalkProposal
+
+
+def get_talk_type_label(value):
+    for talk_type in TalkProposal.TALK_TYPES:
+        if talk_type[0] == value:
+            return talk_type[1]
+
+    return value
+
+
+def talk_proposal_notification(data):
+    title = data['title']
+    name = data['name']
+    talk_type = get_talk_type_label(data['talk_type'])
+    description = data['description']
+    channel = settings.SLACK_TALK_PROPOSAL_CHANNEL
+    text = ':tada: A new talk has been submitted! :tada:'
+    attachment = {
+        'fallback': f'"{title}" has been submitted by {name}',
+        'author_name': name,
+        'title': title,
+        # TODO: Add talk proposal view to frontend and link to it here
+        # "title_link": "https://api.slack.com/",
+        'fields': [
+            {
+                'title': 'Talk Type',
+                'value': talk_type,
+                'short': True,
+            },
+            {
+                'title': 'Description',
+                'value': description,
+                'short': False,
+            }
+        ],
+    }
+    send_message(channel, text, [attachment])
--- nerd_herder/talk_proposals/views.py
+++ nerd_herder/talk_proposals/views.py
@@ -1,19 +1,10 @@
-from django.conf import settings
 from rest_framework import generics, status
 from rest_framework.response import Response
 
 from nerd_herder.permissions import AuthenticatedCanGet
-from nerd_herder.slack.messages import send_message
 from .models import TalkProposal
 from .serializers import TalkProposalSerializer
-
-
-def get_talk_type_label(value):
-    for talk_type in TalkProposal.TALK_TYPES:
-        if talk_type[0] == value:
-            return talk_type[1]
-
-    return value
+from .utils import talk_proposal_notification
 
 
 class TalkProposalList(generics.ListCreateAPIView):
@@ -23,30 +14,5 @@ class TalkProposalList(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = super().create(request, *args, **kwargs).data
-        title = data['title']
-        name = data['name']
-        talk_type = get_talk_type_label(data['talk_type'])
-        description = data['description']
-        channel = settings.SLACK_TALK_PROPOSAL_CHANNEL
-        text = ':tada: A new talk has been submitted! :tada:'
-        attachment = {
-            'fallback': f'"{title}" has been submitted by {name}',
-            'author_name': name,
-            'title': title,
-            # TODO: Add talk proposal view to frontend and link to it here
-            # "title_link": "https://api.slack.com/",
-            'fields': [
-                {
-                    'title': 'Talk Type',
-                    'value': talk_type,
-                    'short': True,
-                },
-                {
-                    'title': 'Description',
-                    'value': description,
-                    'short': False,
-                }
-            ],
-        }
-        send_message(channel, text, [attachment])
+        talk_proposal_notification(data)
         return Response({}, status=status.HTTP_201_CREATED)
