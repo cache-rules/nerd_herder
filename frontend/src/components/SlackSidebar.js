d99924f64df8bb4ffc87c2c6fe88f38550520079
KMSkelton <kmskelton@outlook.com>
2018-10-21 02:04:02 -0700

    added link to slack sign-up

---
+++ frontend/src/components/SlackSidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from '../SlackSidebar.css';
+
+class SlackSidebar extends Component {
+  render() {
+    return (
+      <section className="SlackSidebar">
+      <p>Want to join the PuPPy Slack?</p>
+        <a href="http://slack.pspython.com">
+          <img src={require("../assets/slack_logo.png")} />
+        </a>
+      </section>
+    )
+  }
+}
+
+export default SlackSidebar
