ab89843437e143a80b754a5ecfc1234aaf848de7
Alan Vezina <alan@fancysandwiches.com>
2019-04-07 16:25:07 -0700

    Add created field to talk_proposals admin
    Also sort by created descending.

--- nerd_herder/talk_proposals/admin.py
+++ nerd_herder/talk_proposals/admin.py
@@ -5,7 +5,8 @@ from .models import TalkProposal
 
 class TalkProposalModelAdmin(admin.ModelAdmin):
     model = TalkProposal
-    list_display = ("title", "name", "email", "talk_type", "description")
+    list_display = ("title", "created", "name", "email", "talk_type", "description")
+    ordering = ('-created',)
 
 
 admin.site.register(TalkProposal, TalkProposalModelAdmin)
